e694b23acf40d7c229925313539daab6fbf3e92b
GUchechi <godswilluchendu7@gmail.com>
2022-10-26 00:58:25 +0100

    added fetched posts

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // javascript for index.html
+const container = document.querySelector('.blogs')
+
+
+const renderPosts = async () => {
+    const response = await fetch('http://localhost:3000/posts')
+    const posts = await response.json();
+
+    let template = '';
+    posts.forEach(post => {
+        template += `
+          <div class='post'>
+            <h2>${post.title}</h2>
+            <p><small>${post.likes} likes</small></p>
+            <p>${post.body.slice(0, 200)}</p>
+            <a href='/details.html?id${post.id}'>read more...</a>
+          </div>
+        `
+    })
+
+    container.innerHTML = template;
+
+}
+
+window.addEventListener('DOMContentLoaded', () => renderPosts())
